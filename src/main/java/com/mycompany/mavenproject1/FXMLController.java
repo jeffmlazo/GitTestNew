efc76bf2496b8e1dea8bd6f2c4eee5aead577d73
Jeprox <jeffmlazo@yahoo.com>
2020-08-25 12:03:34 +0800

    Commit for v1.0.0 modification

--- src/main/java/com/mycompany/mavenproject1/FXMLController.java
+++ src/main/java/com/mycompany/mavenproject1/FXMLController.java
@@ -14,7 +14,7 @@ public class FXMLController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("You clicked me! modified v1.0.0");
         label.setText("Hello World!");
     }
     
